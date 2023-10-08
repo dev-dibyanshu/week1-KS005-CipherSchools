aad01d6cb8355b1b7ced982655f1de94ed7a95cc
Dev Dibyanshu <120363000+dev-dibyanshu@users.noreply.github.com>
2023-10-08 23:50:04 +0530

    Create Lecture4(2).cpp

---
+++ Lecture4(2).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a,b;
+    cout<<"give two floting point numner"<<endl;
+    
+    cin>>a>>b;
+    if(a>0.01 || b>a){
+        cout<<"the condition in the if statment is correct"<<endl;
+    }
+    
+    
+    return 0;
+}
