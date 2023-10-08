2f552f234462b61f085f4b37b3f56c97d7e72f1c
Dev Dibyanshu <120363000+dev-dibyanshu@users.noreply.github.com>
2023-10-08 23:53:59 +0530

    Create Lecture6(2).cpp

---
+++ Lecture6(2).cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int i,j,k;
+   
+    i=0;
+    j=0;
+    k=0;
+    cout<<i<<" "<<j<<" "<<k<<endl;
+    j=i++;
+    cout<<i<<" "<<j<<" "<<k<<endl;
+    k=++i;
+    cout<<i<<" "<<j<<" "<<k<<endl;
+    return 0;
+}
+//for loop
+for(i=0;i<101;i++){
+    cout<<i<<" ";
+}
+//while loop
+i=1;//initilizatiom
+while(i<101)//condtion
+{
+    cout<<i;
+    i++;//updation
+}
+//== and =
+int marks;
+i=100;
+for(i=1;i<101;i++){
+    cin>>input
+    if(input==65){
+        cout<<"congrats you have guessed correctlyy!!!!!!;)";
+        break;
+    }
+}
